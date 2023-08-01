801994faaedff5a05237b71ff6006d2a5731b3ba
havardekodehode <havarde.kodehode@gmail.com>
2023-08-01 15:19:01 +0200

    functioning goat screams

--- index.js
+++ index.js
@@ -4,42 +4,43 @@ const mainEl = document.getElementById("main");
 
 const audioPlayer = new Audio;
 
-document.addEventListener("click",(e)=>{
-    // if(e.target.dataset === )
-    switch (e.target.dataset.property) {
-        case "image1":
-            console.log("white goat");
-            break;
-        case "image2":
-            console.log("brown goat");
+// document.addEventListener("click",(e)=>{
+//     if(e.target.dataset === )
+//     switch (e.target.dataset.property) {
+//         case "image1":
+//             console.log("white goat");
+//             // audioPlayer.scr = 
+//             break;
+//         case "image2":
+//             console.log("brown goat");
 
-            break;
-        case  "image3":
+//             break;
+//         case  "image3":
             
-            break;
-        case "image4":
+//             break;
+//         case "image4":
             
-            break;
-        case "image5":
+//             break;
+//         case "image5":
             
-            break;
-        case "image6":
+//             break;
+//         case "image6":
             
-            break;
-        case "image7":
+//             break;
+//         case "image7":
             
-            break;
-        case "image8":
+//             break;
+//         case "image8":
             
-            break;
-        case "image9":
+//             break;
+//         case "image9":
             
-            break;
+//             break;
     
-        default:
-            break;
-    }
-})
+//         default:
+//             break;
+//     }
+// })
 
 function render(){
     const goatContainer = document.createElement("div")
@@ -50,6 +51,12 @@ function render(){
         goatButton.classList = "goatButton"
         goatButton.style.backgroundImage = `url(${goat.image})`
         goatButton.dataset.property = goat.goat
+
+        goatButton.addEventListener("click",()=>{
+            audioPlayer.src=goat.sound
+            audioPlayer.play()
+        })
+
         goatContainer.append(goatButton)
     });
 
