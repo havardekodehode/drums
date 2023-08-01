885a84429b993480ba4948281afe3a1f6971b17b
havardekodehode <havarde.kodehode@gmail.com>
2023-08-01 14:19:01 +0200

    Initial commit, working with indexed for loop

---
+++ data.js
@@ -0,0 +1,47 @@
+export const goats = [
+    {
+        goat: "goat1",
+        image: "images/goat1.png",
+        sound: "sounds/sound1.mp3"
+    },
+    {
+        goat: "goat2",
+        image: "images/goat2.png",
+        sound: "sounds/sound2.mp3"
+    },
+    {
+        goat: "goat3",
+        image: "images/goat3.png",
+        sound: "sounds/sound3.mp3"
+    },
+    {
+        goat: "goat4",
+        image: "images/goat4.png",
+        sound: "sounds/sound4.mp3"
+    },
+    {
+        goat: "goat5",
+        image: "images/goat5.png",
+        sound: "sounds/sound5.mp3"
+    },
+    {
+        goat: "goat6",
+        image: "images/goat6.png",
+        sound: "sounds/sound6.mp3"
+    },
+    {
+        goat: "goat7",
+        image: "images/goat7.png",
+        sound: "sounds/sound7.mp3"
+    },
+    {
+        goat: "goat8",
+        image: "images/goat8.png",
+        sound: "sounds/sound8.mp3"
+    },
+    {
+        goat: "goat9",
+        image: "images/goat9.png",
+        sound: "sounds/sound9.mp3"
+    }
+];
