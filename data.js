edcf62f6feb025c24099c41267fbdcab9382489f
havardekodehode <havarde.kodehode@gmail.com>
2023-08-02 19:42:47 +0200

    added functiong toggles

--- data.js
+++ data.js
@@ -4,116 +4,111 @@ export const drum = [
             goat: "goat1",
             image: "images/goat1.png",
             sound: "sounds/sound1.mp3",
-            key: "s"
+            key: "a",
         },
         {
             goat: "goat2",
             image: "images/goat2.png",
             sound: "sounds/sound2.mp3",
-            key: "d"
+            key: "s",
         },
         {
             goat: "goat3",
             image: "images/goat3.png",
             sound: "sounds/sound3.mp3",
-            key: "f"
+            key: "d",
         },
         {
             goat: "goat4",
             image: "images/goat4.png",
             sound: "sounds/sound4.mp3",
-            key: "g"
+            key: "f",
         },
         {
             goat: "goat5",
             image: "images/goat5.png",
             sound: "sounds/sound5.mp3",
-            key: "h"
+            key: "j",
         },
         {
             goat: "goat6",
             image: "images/goat6.png",
             sound: "sounds/sound6.mp3",
-            key: "i"
+            key: "k",
         },
         {
             goat: "goat7",
             image: "images/goat7.png",
             sound: "sounds/sound7.mp3",
-            key: "j"
+            key: "l",
         },
         {
             goat: "goat8",
             image: "images/goat8.png",
             sound: "sounds/sound8.mp3",
-            key: "k"
+            key: "ø",
         },
         {
             goat: "goat9",
             image: "images/goat9.png",
             sound: "sounds/sound9.mp3",
-            key: "l"
-        }
-    ],[
-
-            {
-                drum: "drum1",
-                image: "images/drum1.png",
-                sound: "sounds/sound1.mp3",
-                key: "s"
-            },
-            {
-                drum: "drum2",
-                image: "images/drum2.png",
-                sound: "sounds/sound2.mp3",
-                key: "d"
-            },
-            {
-                drum: "drum3",
-                image: "images/drum3.png",
-                sound: "sounds/sound3.mp3",
-                key: "f"
-            },
-            {
-                drum: "drum4",
-                image: "images/drum4.png",
-                sound: "sounds/sound4.mp3",
-                key: "g"
-            },
-            {
-                drum: "drum5",
-                image: "images/drum5.png",
-                sound: "sounds/sound5.mp3",
-                key: "h"
-            },
-            {
-                drum: "drum6",
-                image: "images/drum6.png",
-                sound: "sounds/sound6.mp3",
-                key: "i"
-            },
-            {
-                drum: "drum7",
-                image: "images/drum7.png",
-                sound: "sounds/sound7.mp3",
-                key: "j"
-            },
-            {
-                drum: "drum8",
-                image: "images/drum8.png",
-                sound: "sounds/sound8.mp3",
-                key: "k"
-            },
-            {
-                drum: "drum9",
-                image: "images/drum9.png",
-                sound: "sounds/sound9.mp3",
-                key: "l"
-            }
-        
-        
-    ]
-    
-        
+            key: "æ",
+        },
+    ],
+    [
+        {
+            drum: "drum1",
+            image: "images/drum1.png",
+            sound: "sounds/sound1.mp3",
+            key: "a",
+        },
+        {
+            drum: "drum2",
+            image: "images/drum2.png",
+            sound: "sounds/sound2.mp3",
+            key: "s",
+        },
+        {
+            drum: "drum3",
+            image: "images/drum3.png",
+            sound: "sounds/sound3.mp3",
+            key: "d",
+        },
+        {
+            drum: "drum4",
+            image: "images/drum4.png",
+            sound: "sounds/sound4.mp3",
+            key: "f",
+        },
+        {
+            drum: "drum5",
+            image: "images/drum5.png",
+            sound: "sounds/sound5.mp3",
+            key: "j",
+        },
+        {
+            drum: "drum6",
+            image: "images/drum6.png",
+            sound: "sounds/sound6.mp3",
+            key: "k",
+        },
+        {
+            drum: "drum7",
+            image: "images/drum7.png",
+            sound: "sounds/sound7.mp3",
+            key: "l",
+        },
+        {
+            drum: "drum8",
+            image: "images/drum8.png",
+            sound: "sounds/sound8.mp3",
+            key: "ø",
+        },
+        {
+            drum: "drum9",
+            image: "images/drum9.png",
+            sound: "sounds/sound9.mp3",
+            key: "æ",
+        },
+    ],
 ];
-
--- index.js
+++ index.js
@@ -1,50 +1,93 @@
 import { drum } from "./data.js";
 
 const mainEl = document.getElementById("main");
+const keyboard = document.createElement("div");
+keyboard.classList = "keyboard";
+const left = document.createElement("div");
+left.classList = "left";
+const right = document.createElement("div");
+right.classList = "right";
+let keys = document.querySelectorAll(".keyboardKey");
+const radios = document.querySelectorAll('input[name="toggle"]');
 
-async function play(goat, container){
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-
-    const audioPlayer = new Audio;
-    audioPlayer.src=goat.sound
-    audioPlayer.play()
-    container.style.backgroundColor="red"
-    await delay(1000);
-    container.style.backgroundColor="black"
-}
-
-function render(drum, name){
-    const goatContainer = document.createElement("div")
-    goatContainer.classList = name
-
-    drum.forEach(goat => {
-        const containerWKey = document.createElement("div")
-        containerWKey.classList= "buttonContainerWKey"
-        const container = document.createElement("div")
-        container.classList= "buttonContainer"
-        const dekstopKey = document.createElement("div")
-        dekstopKey.classList = "key"
-        dekstopKey.textContent = goat.key
-        // dekstopKey.style.display = "none"
-        const goatButton = document.createElement("button")
-        goatButton.classList = "goatButton"
-        goatButton.style.backgroundImage = `url(${goat.image})`
-        goatButton.dataset.property = goat.goat
-
-        goatButton.addEventListener("click",()=>play(goat, container))
-        document.addEventListener("keypress", async(e) => e.key === goat.key?play(goat, container):null);
-
-
-
-        container.append(goatButton)
-        containerWKey.append(container, dekstopKey)
-        goatContainer.append(containerWKey)
+for (let i = 0; i < radios.length; i++) {
+    radios[i].addEventListener("change", function () {
+        let val = this.value;
+        console.log(val);
+        if (val === "drums") {
+            console.log("drum");
+        } else if (val === "goats") {
+            console.log("goat");
+        }
     });
-
-    mainEl.append(goatContainer)
-
 }
 
-render(drum[0], "goats")
+async function play(goat, container, e) {
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
+    const audioPlayer = new Audio();
+    audioPlayer.src = goat.sound;
+    audioPlayer.play();
+    // const keykey = HTMLElement;
+    console.log(keys);
+    keys.forEach((key) => {
+        if (key.textContent === goat.key) {
+            key.style.backgroundColor = "red";
+        }
+    });
+    container.style.backgroundColor = "red";
+    // keykey.style.backgroundColor = "red";
+    await delay(1000);
+    keys.forEach((key) => {
+        if (key.textContent === goat.key) {
+            key.style.backgroundColor = "gray";
+        }
+    });
+    container.style.backgroundColor = "black";
+}
 
+function render(drum, name) {
+    const goatContainer = document.createElement("div");
+    goatContainer.classList = name;
+
+    drum.forEach((goat) => {
+        const containerWKey = document.createElement("div");
+        containerWKey.classList = "buttonContainerWKey";
+        const container = document.createElement("div");
+        container.classList = "buttonContainer";
+        const dekstopKey = document.createElement("div");
+        dekstopKey.classList = "key";
+        dekstopKey.textContent = goat.key;
+        // dekstopKey.style.display = "none"
+        const goatButton = document.createElement("button");
+        goatButton.classList = "goatButton";
+        goatButton.style.backgroundImage = `url(${goat.image})`;
+        goatButton.dataset.property = goat.goat;
+
+        const keyboardLeft = ["a", "s", "d", "f"];
+        // const keyboardRight = ["j", "k", "l", "ø"];
+        const KBKey = document.createElement("div");
+        KBKey.classList = "keyboardKey";
+        if (keyboardLeft.includes(goat.key)) {
+            KBKey.append((document.createElement("h1").textContent = goat.key));
+            left.append(KBKey);
+        } else {
+            KBKey.append((document.createElement("h1").textContent = goat.key));
+            right.append(KBKey);
+        }
+
+        goatButton.addEventListener("click", () => play(goat, container));
+        document.addEventListener("keypress", async (e) =>
+            e.key === goat.key ? play(goat, container, e) : null
+        );
+
+        container.append(goatButton);
+        containerWKey.append(container, dekstopKey);
+        goatContainer.append(containerWKey);
+    });
+    keyboard.append(left, right);
+    mainEl.append(goatContainer, keyboard);
+    keys = document.querySelectorAll(".keyboardKey");
+}
+
+render(drum[0], "goats");
